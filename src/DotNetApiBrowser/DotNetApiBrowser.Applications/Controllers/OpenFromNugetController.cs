66f739d6a13b9741aec8ba75a84ad0fd8c2708c5
jbe2277 <jbe2277@users.noreply.github.com>
2017-12-26 19:51:56 +0100

    List NuGet packages from server

--- src/DotNetApiBrowser/DotNetApiBrowser.Applications/Controllers/OpenFromNugetController.cs
+++ src/DotNetApiBrowser/DotNetApiBrowser.Applications/Controllers/OpenFromNugetController.cs
@@ -1,4 +1,15 @@
-﻿using System.ComponentModel.Composition;
+﻿using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Waf.Applications;
 using Waf.DotNetApiBrowser.Applications.ViewModels;
 
@@ -19,6 +30,7 @@ namespace Waf.DotNetApiBrowser.Applications.Controllers
             this.openFromNugetViewModel = openFromNugetViewModel;
             this.selectPackageViewModel = selectPackageViewModel;
             this.selectAssemblyViewModel = selectAssemblyViewModel;
+            selectPackageViewModel.PropertyChanged += SelectPackageViewModelPropertyChanged;
             backCommand = new DelegateCommand(Back, CanBack);
             nextCommand = new DelegateCommand(Next, CanNext);
         }
@@ -45,6 +57,53 @@ namespace Waf.DotNetApiBrowser.Applications.Controllers
         private void Next()
         {
             openFromNugetViewModel.Close();
+        }
+
+        private async void SelectPackageViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (new[] { nameof(selectPackageViewModel.SearchText), nameof(selectPackageViewModel.IncludePrerelease) }.Contains(e.PropertyName))
+            {
+                selectPackageViewModel.NugetPackages = await GetNugetPackages(selectPackageViewModel.SearchText, selectPackageViewModel.IncludePrerelease);
+                selectPackageViewModel.SelectedNugetPackage = selectPackageViewModel.NugetPackages.FirstOrDefault();
+                // TODO: error handling
+            }
+            else if (e.PropertyName == nameof(selectPackageViewModel.SelectedNugetPackage))
+            {
+                selectPackageViewModel.PackageVersions = Array.Empty<VersionInfo>();
+                selectPackageViewModel.SelectedPackageVersion = null;
+                selectPackageViewModel.PackageVersions = await GetVersionInfos(selectPackageViewModel.SelectedNugetPackage);
+                selectPackageViewModel.SelectedPackageVersion = selectPackageViewModel.PackageVersions.FirstOrDefault();
+                // TODO: error handling
+            }
+        }
+
+        private async Task<IReadOnlyList<IPackageSearchMetadata>> GetNugetPackages(string searchText, bool includePrerelease)
+        {
+            if (string.IsNullOrEmpty(searchText)) return Array.Empty<IPackageSearchMetadata>();
+            var packageSource = new PackageSource("https://api.nuget.org/v3/index.json");
+            var sourceRepository = new SourceRepository(packageSource, Repository.Provider.GetCoreV3());
+
+            var searchResource = await sourceRepository.GetResourceAsync<PackageSearchResource>().ConfigureAwait(false);
+            return (await searchResource.SearchAsync(searchText, new SearchFilter(includePrerelease), 0, 10, new Logger(), CancellationToken.None).ConfigureAwait(false)).ToArray();
+        }
+
+        private async Task<IReadOnlyList<VersionInfo>> GetVersionInfos(IPackageSearchMetadata packageSearchMetadata)
+        {
+            if (packageSearchMetadata == null) return Array.Empty<VersionInfo>();
+            return (await packageSearchMetadata.GetVersionsAsync().ConfigureAwait(false)).Reverse().ToArray();
+        }
+
+
+        private sealed class Logger : ILogger
+        {
+            public void LogDebug(string data) => Trace.WriteLine($"DEBUG: {data}");
+            public void LogVerbose(string data) => Trace.WriteLine($"VERBOSE: {data}");
+            public void LogInformation(string data) => Trace.WriteLine($"INFORMATION: {data}");
+            public void LogMinimal(string data) => Trace.WriteLine($"MINIMAL: {data}");
+            public void LogWarning(string data) => Trace.WriteLine($"WARNING: {data}");
+            public void LogError(string data) => Trace.WriteLine($"ERROR: {data}");
+            public void LogInformationSummary(string data) => Trace.WriteLine($"INFO SUMMARY: {data}");
+            public void LogErrorSummary(string data) => Trace.WriteLine($"ERROR SUMMARY: {data}");
         }
     }
 }
--- src/DotNetApiBrowser/DotNetApiBrowser.Applications/ViewModels/SelectPackageViewModel.cs
+++ src/DotNetApiBrowser/DotNetApiBrowser.Applications/ViewModels/SelectPackageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.Composition;
+﻿using NuGet.Protocol.Core.Types;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.Waf.Applications;
 using Waf.DotNetApiBrowser.Applications.Views;
 
@@ -7,9 +9,52 @@ namespace Waf.DotNetApiBrowser.Applications.ViewModels
     [Export, PartCreationPolicy(CreationPolicy.NonShared)]
     public class SelectPackageViewModel : ViewModel<ISelectPackageView>
     {
+        private string searchText;
+        private bool includePrerelease;
+        private IReadOnlyList<IPackageSearchMetadata> nugetPackages;
+        private IPackageSearchMetadata selectedNugetPackage;
+        private IReadOnlyList<VersionInfo> packageVersions;
+        private VersionInfo selectedPackageVersion;
+
         [ImportingConstructor]
         public SelectPackageViewModel(ISelectPackageView view) : base(view)
         {
+        }
+        
+        public string SearchText
+        {
+            get { return searchText; }
+            set { SetProperty(ref searchText, value); }
+        }
+        
+        public bool IncludePrerelease
+        {
+            get { return includePrerelease; }
+            set { SetProperty(ref includePrerelease, value); }
+        }
+        
+        public IReadOnlyList<IPackageSearchMetadata> NugetPackages
+        {
+            get { return nugetPackages; }
+            set { SetProperty(ref nugetPackages, value); }
+        }
+        
+        public IPackageSearchMetadata SelectedNugetPackage
+        {
+            get { return selectedNugetPackage; }
+            set { SetProperty(ref selectedNugetPackage, value); }
+        }
+        
+        public IReadOnlyList<VersionInfo> PackageVersions
+        {
+            get { return packageVersions; }
+            set { SetProperty(ref packageVersions, value); }
+        }
+
+        public VersionInfo SelectedPackageVersion
+        {
+            get { return selectedPackageVersion; }
+            set { SetProperty(ref selectedPackageVersion, value); }
         }
     }
 }
