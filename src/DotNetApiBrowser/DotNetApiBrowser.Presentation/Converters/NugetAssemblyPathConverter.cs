e195c67197031a760eadf12c211f7d7ac0cd0310
jbe2277 <jbe2277@users.noreply.github.com>
2017-12-30 14:36:22 +0100

    Unescape nuget package content path

---
+++ src/DotNetApiBrowser/DotNetApiBrowser.Presentation/Converters/NugetAssemblyPathConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Waf.DotNetApiBrowser.Presentation.Converters
+{
+    public class NugetAssemblyPathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Uri.UnescapeDataString((string)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
