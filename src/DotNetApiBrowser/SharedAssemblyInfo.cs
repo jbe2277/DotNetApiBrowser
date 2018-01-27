78dbdfcadf9f98e2d71a2ee58c39b49cf8022861
jbe2277 <jbe2277@users.noreply.github.com>
2018-01-27 22:03:22 +0100

    Prepare first ClickOnce release

--- src/DotNetApiBrowser/SharedAssemblyInfo.cs
+++ src/DotNetApiBrowser/SharedAssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Resources;
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
 
 
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.90")]
+[assembly: AssemblyFileVersion("1.0.0.90")]
