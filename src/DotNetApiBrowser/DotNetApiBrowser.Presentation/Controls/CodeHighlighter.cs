7782394a855bd9d68a90e610919701e1f9c41517
jbe2277 <jbe2277@users.noreply.github.com>
2018-08-08 22:07:20 +0200

    Update WAF to 4.3.0-alpha1

--- src/DotNetApiBrowser/DotNetApiBrowser.Presentation/Controls/CodeHighlighter.cs
+++ src/DotNetApiBrowser/DotNetApiBrowser.Presentation/Controls/CodeHighlighter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Waf.Foundation;
 using ICSharpCode.AvalonEdit.Document;
 using ICSharpCode.AvalonEdit.Highlighting;
 using Microsoft.CodeAnalysis;
