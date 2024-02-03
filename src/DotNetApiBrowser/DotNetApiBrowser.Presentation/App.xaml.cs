455468120edd720751f7aa71038d79e8f29f2c5f
jbe2277 <jbe2277@users.noreply.github.com>
2024-02-03 17:38:31 +0100

    Improve App class

--- src/DotNetApiBrowser/DotNetApiBrowser.Presentation/App.xaml.cs
+++ src/DotNetApiBrowser/DotNetApiBrowser.Presentation/App.xaml.cs
@@ -12,10 +12,9 @@ namespace Waf.DotNetApiBrowser.Presentation;
 
 public partial class App
 {
-    private AggregateCatalog catalog;
-    private CompositionContainer container;
-    private IEnumerable<IModuleController> moduleControllers;
-
+    private AggregateCatalog catalog = null!;
+    private CompositionContainer container = null!;
+    private IEnumerable<IModuleController> moduleControllers = [];
 
     protected override void OnStartup(StartupEventArgs e)
     {
@@ -36,41 +35,30 @@ public partial class App
         batch.AddExportedValue(container);
         container.Compose(batch);
 
-        // Initialize and run all module controllers
         moduleControllers = container.GetExportedValues<IModuleController>();
-        foreach (var moduleController in moduleControllers) { moduleController.Initialize(); }
-        foreach (var moduleController in moduleControllers) { moduleController.Run(); }
+        foreach (var x in moduleControllers) x.Initialize();
+        foreach (var x in moduleControllers) x.Run();
     }
 
     protected override void OnExit(ExitEventArgs e)
     {
-        // Shutdown the module controllers in reverse order
-        foreach (var moduleController in moduleControllers.Reverse()) { moduleController.Shutdown(); }
-
-        // Dispose
+        foreach (var x in moduleControllers.Reverse()) x.Shutdown();
         container.Dispose();
         catalog.Dispose();
         base.OnExit(e);
     }
 
-    private static void AppDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
-    {
-        HandleException(e.Exception, false);
-    }
+    private static void AppDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e) => HandleException(e.Exception, false);
 
-    private static void AppDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
-    {
-        HandleException(e.ExceptionObject as Exception, e.IsTerminating);
-    }
+    private static void AppDomainUnhandledException(object sender, UnhandledExceptionEventArgs e) => HandleException(e.ExceptionObject as Exception, e.IsTerminating);
 
-    private static void HandleException(Exception e, bool isTerminating)
+    private static void HandleException(Exception? e, bool isTerminating)
     {
-        if (e == null) return;
+        if (e is null) return;
         Trace.TraceError(e.ToString());
         if (!isTerminating)
         {
-            MessageBox.Show(string.Format(CultureInfo.CurrentCulture, "Unknown Application Error\n\n{0}", e),
-                ApplicationInfo.ProductName, MessageBoxButton.OK, MessageBoxImage.Error);
+            MessageBox.Show(string.Format(CultureInfo.CurrentCulture, "Unknown Application Error\n\n{0}", e), ApplicationInfo.ProductName, MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
